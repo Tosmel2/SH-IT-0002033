dcd1688660d82c6bbde55542c3207323aab18523
Tosin Adewale <84603888+Tosmel2@users.noreply.github.com>
2022-05-05 23:57:09 +0100

    ecommerce website of SideHustle internship Practice

---
+++ ecommerce_website/components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact us</h3>
+         <a href="tel:234806 8957966"><i class="fas fa-phone"></i> +234 806 895 7966</a>
+         <a href="tel:234806 8957966"><i class="fas fa-phone"></i> +234 806 895 7966</a>
+         <a href="mailto:Tosmelt@gmail.com"><i class="fas fa-envelope"></i> Tosmelt@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i>Lagos, Nigeria</a>
+      </div>
+
+      <div class="box">
+         <h3>Follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="https://www.instagram.com/adnegsdigital"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="https://www.linkedin.com/in/tosin-adewale-4b61041b0/"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Adewale Tosin. SideHustle</span> | all rights reserved!</div>
+
+</footer>
